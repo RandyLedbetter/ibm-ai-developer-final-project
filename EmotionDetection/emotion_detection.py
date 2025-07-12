5cf4d8134c787e53c8b642c823b00f9429e2e878
RandyLedbetter <contact@randyledbetter>
2025-07-12 12:42:35 -0400

    7 - Error Handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,10 +2,27 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    error_payload = {
+        "anger": None,
+        "disgust": None,
+        "fear": None,
+        "joy": None,
+        "sadness": None,
+        "dominant_emotion": None
+    }
+
+    # Handle server error response when the user fails to provide text to analyze
+    if not text_to_analyze:
+        return error_payload
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Handler server error response for status_code 400
+    if response.status_code == 400:
+        return error_payload
 
     formatted_response = json.loads(response.text)
 
@@ -28,5 +45,4 @@ def emotion_detector(text_to_analyze):
 
     # Returning the complete emotion_map dictionary containing all required key value pairs
     return emotion_map
-
-
+    
